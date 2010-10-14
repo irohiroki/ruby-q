a835429304e289e6c2caecca225d8ea53152c8b5
Hiroki Yoshioka <irohiroki@gmail.com>
2010-10-14 23:49:38 +0900

    a pristine rails tree.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyq::Application.config.secret_token = '5d23748acb14c71629d9911a792dd3aabb3f06fde0b4c40721029af64125f3851b2808779362988071a7cd7f516d757d5603e55b3df4ade2ffeae47f81cac0f2'
