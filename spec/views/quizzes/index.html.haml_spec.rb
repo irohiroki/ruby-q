c259f6a9e366b3cbac86acbe3510f1d0d18079fe
Hiroki Yoshioka <irohiroki@gmail.com>
2010-11-08 17:24:07 +0900

    fixes the quizzes/index spec.

--- spec/views/quizzes/index.html.haml_spec.rb
+++ spec/views/quizzes/index.html.haml_spec.rb
@@ -19,10 +19,8 @@ describe "quizzes/index.html.haml" do
   it "renders a list of quizzes" do
     render
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "article>p", :text => "MyText".to_s, :count => 2
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Answer".to_s, :count => 2
-    # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Ruby Version".to_s, :count => 2
+    assert_select "article>p", :text => "Ruby Version".to_s, :count => 2
   end
 end
